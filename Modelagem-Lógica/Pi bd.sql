6064deb9ed685299c653e428f0b3cceb21436b90
mathsprado714 <matheus.prado@sptech.school>
2022-11-19 23:44:05 -0300

    Estruturando o Back-End

--- Modelagem-Lógica/Pi bd.sql
+++ Modelagem-Lógica/Pi bd.sql
@@ -21,12 +21,13 @@ create table notaJogador(
     primary key (idnotaJogador, fkUsuario)
 );
 
-create table timeFavorito(
-	idtimeFavorito int auto_increment,
+create table avaliacao(
+	idAvaliacao int auto_increment,
     fkUsuario int,
     foreign key (fkUsuario) references usuario(idUsuario),
-    primary key (idtimeFavorito, fkUsuario),
-    nomeTime varchar(45)
+    primary key (idAvaliacao, fkUsuario),
+    titulo varchar(45),
+    descricao varchar(255)
 );
 
 select * from usuario as u join
@@ -37,7 +38,124 @@ select u.idUsuario, u.nome, nj.* from usuario as u join
 	notaJogador as nj
 		on nj.fkUsuario = u.idUsuario;
         
-#para mostrar o time favorito
+#para mostrar a avaliação do usuario sobre o site
 
-select u.nome, tm.nomeTime from usuario as u join
-	timeFavorito as tm on tm.fkUsuario = u.idUsuario;
+select u.idUsuario, a.titulo, a.descricao from usuario as u
+	join avaliacao as a on a.fkUsuario = u.idUsuario;
+    
+select * from usuario;
+desc avaliacao;
+SELECT 
+            a.idAvaliacao AS idAviso,
+            a.titulo,
+            a.descricao,
+            a.fkUsuario,
+            u.idUsuario AS idUsuario,
+            u.nome,
+            u.email,
+            u.senha
+        FROM avaliacao a
+            INNER JOIN usuario u
+                ON a.fkUsuario = u.idUsuario
+        WHERE u.idUsuario = idUsuario;
+        
+SELECT 
+            a.idAvaliacao AS idAviso,
+            a.titulo,
+            a.descricao,
+            a.fkUsuario,
+            u.idUsuario AS idUsuario,
+            u.nome,
+            u.email,
+            u.senha
+        FROM avaliacao a
+            INNER JOIN usuario u
+                ON a.fkUsuario = u.idUsuario
+        WHERE a.descricao LIKE '${texto}';
+        
+SELECT 
+            a.idAvaliacao AS idAviso,
+            a.titulo,
+            a.descricao,
+            a.fkUsuario,
+            u.idUsuario AS idUsuario,
+            u.nome,
+            u.email,
+            u.senha
+        FROM avaliacao a
+            INNER JOIN usuario u
+                ON a.fkUsuario = u.idUsuario;
+                
+##################################################
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+
+
+/*
+comando para sql server - banco remoto - ambiente de produção
+*/
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
+);
+
+create table aquario (
+/* em nossa regra de negócio, um aquario tem apenas um sensor */
+	id INT PRIMARY KEY IDENTITY(1,1),
+	descricao VARCHAR(300)
+);
+
+/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+
+CREATE TABLE medida (
+	id INT PRIMARY KEY IDENTITY(1,1),
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
+);
--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_DATABASE",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "premier",
+    user: "root",
+    password: "loroinfante",
 };
 
 function executar(instrucao) {
