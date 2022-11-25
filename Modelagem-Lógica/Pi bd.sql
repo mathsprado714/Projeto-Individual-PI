db9c4135fd0e97e47517a77028b70e23618f451a
mathsprado714 <matheus.prado@sptech.school>
2022-11-25 00:50:01 -0300

    Adicionando videos explicativos nas pags dos times

--- Modelagem-Lógica/Pi bd.sql
+++ Modelagem-Lógica/Pi bd.sql
@@ -64,6 +64,8 @@ select n.nota, j.nomeJogador, j.timeJogador from nota as n join jogador as j on 
 
 desc jogador;
 desc avaliacao;
+
+#selects usadoa para buscar a avaliação do usuário
 SELECT 
             a.idAvaliacao AS idAviso,
             a.titulo,
@@ -104,41 +106,8 @@ SELECT
         FROM avaliacao a
             INNER JOIN usuario u
                 ON a.fkUsuario = u.idUsuario;
-                
-##################################################
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
+##############
 select * from nota;
+select * from usuario;
+select * from avaliacao;
+select * from jogador;
